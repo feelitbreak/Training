d92b787e0814f28989d8bdc14e8f9cbf6c5ccbab
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-02 21:09:17 +0300

    1.2 1

---
+++ C#/1.2 Style Issues/SA1507/Math.cs
@@ -0,0 +1,12 @@
+﻿namespace StyleIssues.SA1507
+{
+    public static class Math
+    {
+        public static int Sum(int x, int y)
+        {
+            int result = x + y;
+
+            return result;
+        }
+    }
+}
