6b357427920275321ca3c3b9a246c30a8b44fd7f
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-04 15:40:34 +0300

    2.2

---
+++ C#/2.2 Real Numbers/Decimals.cs
@@ -0,0 +1,35 @@
+﻿namespace Literals
+{
+    public static class Decimals
+    {
+        public static decimal ReturnDecimal51()
+        {
+            return 0.0m;
+        }
+
+        public static decimal ReturnDecimal52()
+        {
+            return 0.000_000_1m;
+        }
+
+        public static decimal ReturnDecimal53()
+        {
+            return -10_000.000_000_000_1m;
+        }
+
+        public static decimal ReturnDecimal54()
+        {
+            return 1_048_294_829_438_549_029_840_452_834.109_492_298_482m;
+        }
+
+        public static decimal ReturnDecimal55()
+        {
+            return -30_492_996_837_502_378_502_387_459_850.942_692_284_652_825m;
+        }
+
+        public static decimal ReturnDecimal56()
+        {
+            return 0.6m + 0.1m;
+        }
+    }
+}
---
+++ C#/2.2 Real Numbers/Doubles.cs
@@ -0,0 +1,35 @@
+﻿namespace Literals
+{
+    public static class Doubles
+    {
+        public static double ReturnDouble41()
+        {
+            return 0.0;
+        }
+
+        public static double ReturnDouble42()
+        {
+            return 0.000_1;
+        }
+
+        public static double ReturnDouble43()
+        {
+            return -10_000.000_000_000_1;
+        }
+
+        public static double ReturnDouble44()
+        {
+           return 1_048_294_829_438_549_029_840_452_834.109_492_298_482;
+        }
+
+        public static double ReturnDouble45()
+        {
+            return -30_492_996_837_502_378_502_387_459_850_243.942_692_284_652_825;
+        }
+
+        public static double ReturnDouble46()
+        {
+            return 0.6 + 0.1;
+        }
+    }
+}
