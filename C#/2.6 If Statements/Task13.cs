b04747f9d8369968c818e6657534af1bf1b0866b
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-04 22:04:58 +0300

    2.6

---
+++ C#/2.6 If Statements/Task13.cs
@@ -0,0 +1,53 @@
+﻿namespace IfStatements
+{
+    public static class Task13
+    {
+        public static int DoSomething(bool b, int i)
+        {
+            if (b)
+            {
+                if (i < -8)
+                {
+                    return 5 + i;
+                }
+
+                if (i >= -8 && i < -4)
+                {
+                    return i;
+                }
+
+                if (i >= -4 && i < 0)
+                {
+                    return 5 + i;
+                }
+
+                if (i == 0)
+                {
+                    return 10;
+                }
+
+                if (i > 0 && i <= 3)
+                {
+                    return i - 5;
+                }
+
+                if (i > 3)
+                {
+                    return i * -1;
+                }
+            }
+
+            if (i <= -5)
+            {
+                return i * -1;
+            }
+
+            if (i > -5 && i <= 5)
+            {
+                return 10 - i;
+            }
+
+            return i * -1;
+        }
+    }
+}
---
+++ C#/2.6 If Statements/Task14.cs
@@ -0,0 +1,69 @@
+﻿namespace IfStatements
+{
+    public static class Task14
+    {
+        public static int DoSomething(bool b1, bool b2, int i)
+        {
+            if (b1 && b2)
+            {
+                if (i <= -5 || i > 5)
+                {
+                    return 10 - (i * 2);
+                }
+
+                if (i > -5 && i <= 5)
+                {
+                    return i * -2;
+                }
+            }
+
+            if (b1 && !b2)
+            {
+                if (i <= -5 || i > 5)
+                {
+                    return i * i * i;
+                }
+
+                if (i > -5 && i <= 5)
+                {
+                    return i * i;
+                }
+            }
+
+            if (!b1 && b2)
+            {
+                if (i < -9 || i > 7)
+                {
+                    return i * -1;
+                }
+
+                if ((i >= -9 && i < -7) || (i >= -3 && i <= 7))
+                {
+                    return i;
+                }
+
+                if (i >= -7 && i < -3)
+                {
+                    return i * 10;
+                }
+            }
+
+            if (i < -9 || i > 7)
+            {
+                return i * -1;
+            }
+
+            if ((i >= -9 && i < -3) || (i >= 5 && i <= 7))
+            {
+                return i;
+            }
+
+            if ((i >= -3 && i < 0) || (i > 0 && i < 5))
+            {
+                return i * -100;
+            }
+
+            return 0;
+        }
+    }
+}
