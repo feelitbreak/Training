945cc835a887bf681d09acdfe2844ad0e3b08804
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-04 14:27:12 +0300

    2.1 1

---
+++ C#/2.1 Integers/LongIntegers.cs
@@ -0,0 +1,67 @@
+﻿// ReSharper disable ConvertToConstant.Local
+
+namespace Literals
+{
+    public static class LongIntegers
+    {
+        public static long ReturnLongInteger21()
+        {
+            return 4_956_185_095_298_947_214L;
+        }
+
+        public static long ReturnLongInteger22()
+        {
+            return -1_280_010_762_458_239_942L;
+        }
+
+        public static long ReturnLongInteger23()
+        {
+            return -945_783_496_234_828_465L;
+        }
+
+        public static ulong ReturnLongInteger24()
+        {
+            return 16_269_823_234_523_742_845uL;
+        }
+
+        public static long ReturnLongInteger25()
+        {
+            return 0x7FFF_FFFF_FFFF_FFFFL;
+        }
+
+        public static long ReturnLongInteger26()
+        {
+            return 0x0ABC_DEFA_BCDE_FABCL;
+        }
+
+        public static ulong ReturnLongInteger27()
+        {
+            return 0xF97C_39E7_17FA_63BDuL;
+        }
+
+        public static ulong ReturnLongInteger28()
+        {
+            return 0xC4F7_E62B_7A90_A0BFuL;
+        }
+
+        public static long ReturnLongInteger29()
+        {
+            return 0b11_1000_1110_1000_1101_0101_0011_1010_1011_1010_0110_1010_1001_1010_1001_0101L;
+        }
+
+        public static long ReturnLongInteger210()
+        {
+            return 0b1_0110_1101_0100_1001_0100_0001_0101_0111_0111_0100_1111_0000_1110_1001_0101L;
+        }
+
+        public static long ReturnLongInteger211()
+        {
+            return 0b101_0101_0101_0101_0101_0101_0101_0101_0101_0101_0101_0101_0101_0101_0101_0101L;
+        }
+
+        public static long ReturnLongInteger212()
+        {
+            return 0b111_0101_0011_1000_1101_1100_1111_1011_0111_0110_0001_0111_1111_1111_1111_1111L;
+        }
+    }
+}
