bd50000aa17e9b502ceee867269f3fbf821eb701
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-05 19:48:39 +0300

    2.10

---
+++ C#/2.10 Print face/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PrintFace
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("  Hello, world! ");
+        }
+
+        public static void SayHelloUser(string userName)
+        {
+            Console.WriteLine($"Hello, {userName}!");
+        }
+
+        public static void PrintFace()
+        {
+            Console.WriteLine($" +\"\"\"\"\"+{Environment.NewLine}(| o o |){Environment.NewLine}" +
+                $" |  ^  |{Environment.NewLine} | '-' |{Environment.NewLine} +-----+");
+        }
+    }
+}
