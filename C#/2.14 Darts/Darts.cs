6d9eb3e13f0c273b8a73a75fcfc1c5f9471d9383
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-06 12:28:05 +0300

    2.14

---
+++ C#/2.14 Darts/Darts.cs
@@ -0,0 +1,16 @@
+﻿namespace DartsGame
+{
+    public static class Darts
+    {
+        public static int GetScore(double x, double y)
+        {
+            return (x, y) switch
+            {
+                { } when (x * x) + (y * y) <= 1 => 10,
+                { } when (x * x) + (y * y) <= 25 => 5,
+                { } when (x * x) + (y * y) <= 100 => 1,
+                _ => 0
+            };
+        }
+    }
+}
