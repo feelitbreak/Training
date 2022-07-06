06aa33d87ffce6b39499a57e774b918af4fc5d87
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-06 19:57:23 +0300

    4.1

---
+++ C#/4.1 Arithmetic Sequence/ArithmeticSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArithmeticSequenceTask
+{
+    public static class ArithmeticSequence
+    {
+        public static int Calculate(int number, int add, int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("The count of elements of the sequence cannot be less or equals zero.", nameof(count));
+            }
+
+            if ((number == int.MaxValue && add > 0) || (number == int.MinValue && add < 0))
+            {
+                throw new OverflowException("The count of elements of the sequence cannot be less or equals zero.");
+            }
+
+            int sum = number;
+            int i = 1;
+
+            while (i < count)
+            {
+                sum += number + (i * add);
+                i++;
+            }
+
+            return sum;
+        }
+    }
+}
