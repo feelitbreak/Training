1e1998af55aef9a9163bfb0849ca475196feaf0d
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-07 19:30:51 +0300

    4.4

---
+++ C#/4.4 Gcd/IntegerExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GcdTask
+{
+    public static class IntegerExtensions
+    {
+        public static int FindGcd(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+            else if (b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(b), $"Number cannot be {int.MinValue}.");
+            }
+
+            if (a == 0)
+            {
+                return Math.Abs(b);
+            }
+            else if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return Math.Abs(a);
+        }
+    }
+}
