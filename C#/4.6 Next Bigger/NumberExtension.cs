60bf475e4e5f3627d990d916f7e4d7bbbbf811a3
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-09 23:46:33 +0300

    4.6 added summary

--- C#/4.6 Next Bigger/NumberExtension.cs
+++ C#/4.6 Next Bigger/NumberExtension.cs
@@ -5,6 +5,14 @@ namespace NextBiggerTask
 {
     public static class NumberExtension
     {
+        /// <summary>
+        /// Finds the nearest largest integer consisting of the digits of the given positive integer number and null if no such number exists.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>
+        /// The nearest largest integer consisting of the digits  of the given positive integer and null if no such number exists.
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown when source number is less than 0.</exception>
         public static int? NextBiggerThan(int number)
         {
             if (number < 0)
