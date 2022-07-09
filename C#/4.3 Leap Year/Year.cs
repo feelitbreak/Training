b6028c7db11870eba3ffe6152c1f6fa89ade202f
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-09 23:21:32 +0300

    4.3 added summary

--- C#/4.3 Leap Year/Year.cs
+++ C#/4.3 Leap Year/Year.cs
@@ -4,6 +4,11 @@ namespace LeapYearTask
 {
     public static class Year
     {
+        /// <summary>
+        /// Report if given year is a leap year.
+        /// </summary>
+        /// <param name="year">Given year.</param>
+        /// <returns>True if given year is leap, false otherwise.</returns>
         public static bool IsLeapYear(int year)
         {
             if (year % 4 != 0)
