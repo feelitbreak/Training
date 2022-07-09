dcfee03e31ebff2a655ef3782801aa121e730c68
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-09 23:19:19 +0300

    4.2 added summary

--- C#/4.2 Calculations/Calculator.cs
+++ C#/4.2 Calculations/Calculator.cs
@@ -4,6 +4,11 @@ namespace Calculations
 {
     public static class Calculator
     {
+        /// <summary>
+        /// Calculate the following sum 1/1 + 1/2 + 1/3 + ... + 1/n, where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumOne(int n)
         {
             double sum = 0;
@@ -16,6 +21,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/(1*2) - 1/(2*3) + 1/(3*4) + ... + (-1)^(n+1) / (n * (n + 1)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumTwo(int n)
         {
             double sum = 0;
@@ -28,6 +39,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/1^5 + 1/2^5 + 1/3^5 + ... + 1/n^5, where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumThree(int n)
         {
             const double pow = 5;
@@ -41,6 +58,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/(3 * 3) + 1/(5 * 5) + 1/(7 * 7) + ... + 1/((2 * n + 1) * (2 * n + 1)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumFour(int n)
         {
             double sum = 0;
@@ -53,6 +76,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following product
+        /// (1 + 1/(1 * 1)) * (1 + 1/(2 * 2)) * (1 + 1/(3 * 3)) * ... * (1 + 1/(n * n)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Product of elements.</returns>
         public static double GetProductOne(int n)
         {
             double prod = 1;
@@ -65,6 +94,12 @@ namespace Calculations
             return prod;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// -1/3 + 1/5 - 1/7 + ... + (-1)^n / (2 * n + 1), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumFive(int n)
         {
             double sum = 0;
@@ -77,6 +112,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// 1!/1 + 2!/(1+1/2) + 3!/(1+1/2+1/3) + ... + 1*2*...* n/ (1+1/2+1/3+...+1/n), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumSix(int n)
         {
             double sum = 0;
@@ -93,6 +134,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// Sqrt(2 + Sqrt(2 + ... + Sqrt(2))), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumSeven(int n)
         {
             double sum = 0;
@@ -105,6 +152,12 @@ namespace Calculations
             return sum;
         }
 
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/sin(1) + 1/(sin(1)+sin(2)) + ...+  1/(sin(1)+sin(2)+...+sin(n)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
         public static double GetSumEight(int n)
         {
             double sum = 0;
