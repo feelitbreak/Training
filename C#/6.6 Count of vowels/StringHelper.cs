2d2df033c180e0729e337de48b1f7e47b81a4286
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-13 15:12:10 +0300

    6.6

---
+++ C#/6.6 Count of vowels/StringHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VowelCountTask
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Calculates the count of vowels in the source string.
+        ///  'a', 'e', 'i', 'o', and 'u' are vowels.
+        /// </summary>
+        /// <param name="source">Source string.</param>
+        /// <returns>Count of vowels in the given string.</returns>
+        /// <exception cref="ArgumentException">Thrown when source string is null or empty.</exception>
+        public static int GetCountOfVowel(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentException("Source string is null or empty", nameof(source));
+            }
+
+            int count = 0;
+            for (int i = 0; i < source.Length; i++)
+            {
+                switch (source[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        count++;
+                        break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
