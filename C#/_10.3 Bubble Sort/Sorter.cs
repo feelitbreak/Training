114195025529165715aef0c80e0d91bca4eb16ca
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-27 14:55:43 +0300

    10.3

---
+++ C#/_10.3 Bubble Sort/Sorter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+namespace BubbleSort
+{
+    public static class Sorter
+    {
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with bubble sort algorithm.
+        /// </summary>
+        public static void BubbleSort(this int[]? array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array.SwapUntil(array.Length - i);
+            }
+        }
+
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with recursive bubble sort algorithm.
+        /// </summary>
+        public static void RecursiveBubbleSort(this int[]? array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            array.RecursiveBubbleSort(0);
+        }
+
+        private static void RecursiveBubbleSort(this int[] array, int i)
+        {
+            if (i >= array.Length - 1)
+            {
+                return;
+            }
+
+            array.SwapUntil(array.Length - i);
+
+            array.RecursiveBubbleSort(i + 1);
+        }
+
+        private static void SwapUntil(this int[] array, int limit)
+        {
+            for (int j = 1; j < limit; j++)
+            {
+                if (array[j - 1] > array[j])
+                {
+                    (array[j - 1], array[j]) = (array[j], array[j - 1]);
+                }
+            }
+        }
+    }
+}
