b33a2056aafcabfc4e77466a3c480fba66de1a79
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-28 17:34:52 +0300

    10.1 fixed

--- C#/_10.1 Selection Sort/Sorter.cs
+++ C#/_10.1 Selection Sort/Sorter.cs
@@ -51,7 +51,7 @@ namespace SelectionSort
         private static int FindIndexOfMin(int[] array, int i)
         {
             int jMin = i;
-            for (int j = i; j < array.Length; j++)
+            for (int j = i + 1; j < array.Length; j++)
             {
                 if (array[j] < array[jMin])
                 {
