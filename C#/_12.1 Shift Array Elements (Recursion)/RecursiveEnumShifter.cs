eb13ea031e47f280df9750feb8fe16d5b59a3ca5
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-08-01 23:27:51 +0300

    12.1

---
+++ C#/_12.1 Shift Array Elements (Recursion)/RecursiveEnumShifter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class RecursiveEnumShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using directions from <see cref="directions"/> array, one element shift per each direction array element.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="directions">An array with directions.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">directions array is null.</exception>
+        /// <exception cref="InvalidOperationException">direction array contains an element that is not <see cref="Direction.Left"/> or <see cref="Direction.Right"/>.</exception>
+        public static int[] Shift(int[]? source, Direction[]? directions)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (directions is null)
+            {
+                throw new ArgumentNullException(nameof(directions));
+            }
+
+            return Shift(source, directions, 0);
+        }
+
+        private static int[] Shift(int[] source, Direction[] directions, int i)
+        {
+            if (i >= directions.Length)
+            {
+                return source;
+            }
+
+            switch (directions[i])
+            {
+                case Direction.Left:
+                    {
+                        source.LeftShift();
+                        break;
+                    }
+
+                case Direction.Right:
+                    {
+                        source.RightShift();
+                        break;
+                    }
+
+                default:
+                    throw new InvalidOperationException($"Incorrect {directions[i]} enum value.");
+            }
+
+            return Shift(source, directions, i + 1);
+        }
+
+        private static void LeftShift(this int[] source)
+        {
+            int firstElem = source[0];
+            Array.Copy(source, 1, source, 0, source.Length - 1);
+            source[^1] = firstElem;
+        }
+
+        private static void RightShift(this int[] source)
+        {
+            int lastElem = source[^1];
+            Array.Copy(source, 0, source, 1, source.Length - 1);
+            source[0] = lastElem;
+        }
+    }
+}
---
+++ C#/_12.1 Shift Array Elements (Recursion)/RecursiveShifter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class RecursiveShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using <see cref="iterations"/> array for getting directions and iterations (odd elements - left direction, even elements - right direction).
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="iterations">An array with iterations.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">iterations array is null.</exception>
+        public static int[] Shift(int[]? source, int[]? iterations)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (iterations is null)
+            {
+                throw new ArgumentNullException(nameof(iterations));
+            }
+
+            if (source.Length == 0 || source.Length == 1)
+            {
+                return source;
+            }
+
+            return Shift(source, iterations, 0);
+        }
+
+        private static int[] Shift(int[] source, int[] iterations, int i)
+        {
+            if (i >= iterations.Length)
+            {
+                return source;
+            }
+
+            if (i % 2 == 0)
+            {
+                int numberOfTimes = iterations[i];
+                for (int j = 0; j < numberOfTimes; j++)
+                {
+                    source.LeftShift();
+                }
+            }
+            else
+            {
+                int numberOfTimes = iterations[i];
+                for (int j = 0; j < numberOfTimes; j++)
+                {
+                    source.RightShift();
+                }
+            }
+
+            return Shift(source, iterations, i + 1);
+        }
+
+        private static void LeftShift(this int[] source)
+        {
+            for (int j = 1; j < source.Length; j++)
+            {
+                (source[j - 1], source[j]) = (source[j], source[j - 1]);
+            }
+        }
+
+        private static void RightShift(this int[] source)
+        {
+            for (int j = source.Length - 1; j > 0; j--)
+            {
+                (source[j - 1], source[j]) = (source[j], source[j - 1]);
+            }
+        }
+    }
+}
