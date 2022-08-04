6472ebbcc8d11e892e4d0caa69a9c456ad05c7e5
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-08-04 23:40:35 +0300

    12.5

---
+++ C#/_12.5 Looking for Array Elements (Recursion)/DecimalCounter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace LookingForArrayElements
+{
+    public static class DecimalCounter
+    {
+        /// <summary>
+        /// Searches an array of decimals for elements that are in a specified range, and returns the number of occurrences of the elements that matches the range criteria.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of single-precision floating-point numbers.</param>
+        /// <param name="ranges">One-dimensional, zero-based <see cref="Array"/> of range arrays.</param>
+        /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
+        public static int GetDecimalsCount(decimal[]? arrayToSearch, decimal[]?[]? ranges)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (ranges is null)
+            {
+                throw new ArgumentNullException(nameof(ranges));
+            }
+
+            for (int k = 0; k < ranges.Length; k++)
+            {
+                if (ranges[k] is null)
+                {
+                    throw new ArgumentNullException(nameof(ranges));
+                }
+                else if (ranges[k]!.Length != 0 && ranges[k]!.Length != 2)
+                {
+                    throw new ArgumentException("The length of one of the ranges is less or greater than 2 and not empty.");
+                }
+            }
+
+            return GetDecimalsCountRecursion(arrayToSearch, ranges!, 0, 0);
+        }
+
+        /// <summary>
+        /// Searches an array of decimals for elements that are in a specified range, and returns the number of occurrences of the elements that matches the range criteria.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of single-precision floating-point numbers.</param>
+        /// <param name="ranges">One-dimensional, zero-based <see cref="Array"/> of range arrays.</param>
+        /// <param name="startIndex">The zero-based starting index of the search.</param>
+        /// <param name="count">The number of elements in the section to search.</param>
+        /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
+        public static int GetDecimalsCount(decimal[]? arrayToSearch, decimal[]?[]? ranges, int startIndex, int count)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (ranges is null)
+            {
+                throw new ArgumentNullException(nameof(ranges));
+            }
+
+            for (int k = 0; k < ranges.Length; k++)
+            {
+                if (ranges[k] is null)
+                {
+                    throw new ArgumentNullException(nameof(ranges));
+                }
+                else if (ranges[k]!.Length != 0 && ranges[k]!.Length != 2)
+                {
+                    throw new ArgumentException("The length of one of the ranges is less or greater than 2 and not empty.");
+                }
+            }
+
+            if (startIndex < 0 || startIndex >= arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (count < 0 || count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            return GetDecimalsCountRecursion(arrayToSearch, ranges!, startIndex, 0, startIndex + count);
+        }
+
+        private static int GetDecimalsCountRecursion(decimal[] arrayToSearch, decimal[][] ranges, int i, int j)
+        {
+            if (i == arrayToSearch.Length)
+            {
+                return 0;
+            }
+
+            if (j == ranges.Length)
+            {
+                return GetDecimalsCountRecursion(arrayToSearch, ranges, i + 1, 0);
+            }
+
+            if (ranges[j].Length == 0 || arrayToSearch[i] < ranges[j][0] || arrayToSearch[i] > ranges[j][1])
+            {
+                return GetDecimalsCountRecursion(arrayToSearch, ranges, i, j + 1);
+            }
+            else
+            {
+                return GetDecimalsCountRecursion(arrayToSearch, ranges, i + 1, 0) + 1;
+            }
+        }
+
+        private static int GetDecimalsCountRecursion(decimal[] arrayToSearch, decimal[][] ranges, int i, int j, int limit)
+        {
+            if (i == limit)
+            {
+                return 0;
+            }
+
+            if (j == ranges.Length)
+            {
+                return GetDecimalsCountRecursion(arrayToSearch, ranges, i + 1, 0, limit);
+            }
+
+            if (ranges[j].Length == 0 || arrayToSearch[i] < ranges[j][0] || arrayToSearch[i] > ranges[j][1])
+            {
+                return GetDecimalsCountRecursion(arrayToSearch, ranges, i, j + 1, limit);
+            }
+            else
+            {
+                return GetDecimalsCountRecursion(arrayToSearch, ranges, i + 1, 0, limit) + 1;
+            }
+        }
+    }
+}
---
+++ C#/_12.5 Looking for Array Elements (Recursion)/IntegersCounter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace LookingForArrayElements
+{
+    public static class IntegersCounter
+    {
+        /// <summary>
+        /// Searches an array of integers for elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>, and returns the number of occurrences of the elements.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of integers to search.</param>
+        /// <param name="elementsToSearchFor">One-dimensional, zero-based <see cref="Array"/> that contains integers to search for.</param>
+        /// <returns>The number of occurrences of the elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>.</returns>
+        public static int GetIntegersCount(int[]? arrayToSearch, int[]? elementsToSearchFor)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (elementsToSearchFor is null)
+            {
+                throw new ArgumentNullException(nameof(elementsToSearchFor));
+            }
+
+            return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, 0, 0);
+        }
+
+        /// <summary>
+        /// Searches an array of integers for elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>, and returns the number of occurrences of the elements withing the range of elements in the <see cref="Array"/> that starts at the specified index and contains the specified number of elements.
+        /// </summary>
+        /// <param name="arrayToSearch">One-dimensional, zero-based <see cref="Array"/> of integers to search.</param>
+        /// <param name="elementsToSearchFor">One-dimensional, zero-based <see cref="Array"/> that contains integers to search for.</param>
+        /// <param name="startIndex">The zero-based starting index of the search.</param>
+        /// <param name="count">The number of elements in the section to search.</param>
+        /// <returns>The number of occurrences of the elements that are in <paramref name="elementsToSearchFor"/> <see cref="Array"/>.</returns>
+        public static int GetIntegersCount(int[]? arrayToSearch, int[]? elementsToSearchFor, int startIndex, int count)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (elementsToSearchFor is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (startIndex < 0 || startIndex >= arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (count < 0 || count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, startIndex, 0, startIndex + count);
+        }
+
+        private static int GetIntegersCountRecursion(int[] arrayToSearch, int[] elementsToSearchFor, int i, int j)
+        {
+            if (i == arrayToSearch.Length)
+            {
+                return 0;
+            }
+
+            if (j == elementsToSearchFor.Length)
+            {
+                return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, i + 1, 0);
+            }
+
+            if (arrayToSearch[i] == elementsToSearchFor[j])
+            {
+                return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, i + 1, 0) + 1;
+            }
+
+            return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, i, j + 1);
+        }
+
+        private static int GetIntegersCountRecursion(int[] arrayToSearch, int[] elementsToSearchFor, int i, int j, int limit)
+        {
+            if (i == limit)
+            {
+                return 0;
+            }
+
+            if (j == elementsToSearchFor.Length)
+            {
+                return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, i + 1, 0, limit);
+            }
+
+            if (arrayToSearch[i] == elementsToSearchFor[j])
+            {
+                return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, i + 1, 0, limit) + 1;
+            }
+
+            return GetIntegersCountRecursion(arrayToSearch, elementsToSearchFor, i, j + 1, limit);
+        }
+    }
+}
