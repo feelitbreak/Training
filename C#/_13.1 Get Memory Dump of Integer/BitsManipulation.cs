630bc534b14d6b71af467b6a35f4a69aca7d209f
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-08-06 18:34:14 +0300

    13.1

---
+++ C#/_13.1 Get Memory Dump of Integer/BitsManipulation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BinaryRepresentation
+{
+    public static class BitsManipulation
+    {
+        /// <summary>
+        /// Get binary memory representation of signed long integer.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>Binary memory representation of signed long integer.</returns>
+        public static string GetMemoryDumpOf(long number)
+        {
+            const int bitSize = 64;
+            char[] bytes = new char[bitSize];
+
+            long i = 1;
+            for (int pos = bytes.Length - 1; pos >= 0; pos--, i = MultiplyByTwo(i))
+            {
+                if ((number & i) != 0)
+                {
+                    bytes[pos] = '1';
+                }
+                else
+                {
+                    bytes[pos] = '0';
+                }
+            }
+
+            return new string(bytes);
+        }
+
+        private static long MultiplyByTwo(long num)
+        {
+            return num << 1;
+        }
+    }
+}
