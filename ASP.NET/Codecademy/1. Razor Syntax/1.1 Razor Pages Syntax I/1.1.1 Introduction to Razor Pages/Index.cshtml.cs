cd33a1cb3615ac342e8fb4c95f65f021b60a1493
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-06-25 18:08:49 +0300

    codecademy razor pages syntax part 1

---
+++ ASP.NET/Codecademy/1. Razor Syntax/1.1 Razor Pages Syntax I/1.1.1 Introduction to Razor Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LearnRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string? Greet { get; set; }
+
+        public void OnGet()
+        {
+            Greet = "Hey there!";
+        }
+    }
+}
