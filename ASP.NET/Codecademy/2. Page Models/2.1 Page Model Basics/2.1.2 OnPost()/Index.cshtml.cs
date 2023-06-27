65c9453246f5ff202407b4f26c72cb0269b08aa5
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-06-27 17:44:00 +0300

    codecademy page model basics

---
+++ ASP.NET/Codecademy/2. Page Models/2.1 Page Model Basics/2.1.2 OnPost()/Index.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string RequestMethod
+        { get; set; } = string.Empty;
+
+        public string RequestValues
+        { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Body { get; set; } = string.Empty;
+
+        public void OnGet()
+        {
+            // For debugging
+            RequestMethod = "GET";
+            RequestValues = "n/a";
+
+            // Assign property values here
+            Title = "Cuban Midnight Sandwich";
+            Date = new DateTime(2000, 3, 24);
+            Body = "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
+        }
+
+        public void OnPost(string title, DateTime date, string body)
+        {
+            // For debugging
+            RequestMethod = "POST";
+            RequestValues = GetFormValues();
+
+            // Assign property values here
+            Title = title;
+            Date = date;
+            Body = body;
+        }
+
+        // For debugging
+        private string GetFormValues(bool ignoreRequestVerificationToken = true)
+        {
+            string formData = "";
+            string separator = " | ";
+
+            foreach (var pair in this.Request.Form)
+            {
+                if (ignoreRequestVerificationToken && pair.Key == "__RequestVerificationToken")
+                {
+                    continue;
+                }
+                else
+                {
+                    formData += pair.Key + ": " + Request.Form[pair.Key] + separator;
+                }
+            }
+
+            if (formData.EndsWith(separator))
+            {
+                formData = formData.Substring(0, formData.Length - separator.Length);
+            }
+
+            return formData;
+        }
+    }
+}
---
+++ ASP.NET/Codecademy/2. Page Models/2.1 Page Model Basics/2.1.7 OnPostAsync() (full Blog project)/Pages/Index.cshtml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string RequestMethod
+        { get; set; } = string.Empty;
+
+        public string RequestValues
+        { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Title { get; set; } = string.Empty;
+
+        [BindProperty]
+        public DateTime Date { get; set; }
+
+        [BindProperty]
+        public string Body { get; set; } = string.Empty;
+
+        public async Task OnGetAsync()
+        {
+            // For debugging
+            RequestMethod = "GET";
+            RequestValues = "n/a";
+
+            // Assign property values here
+            Title = "Cuban Midnight Sandwich";
+            Date = new DateTime(2000, 3, 24);
+            Body = "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
+
+            // Asynchronous task
+            using StreamWriter writer = new("log.txt", append: true);
+            await writer.WriteLineAsync($"OnGetAsync() called at {DateTime.Now}.");
+        }
+
+        public async Task OnPostAsync()
+        {
+            // For debugging
+            RequestMethod = "POST";
+            RequestValues = GetFormValues();
+
+            using StreamWriter writer = new("log.txt", append: true);
+            await writer.WriteLineAsync($"OnPostAsync() called at {DateTime.Now}.");
+        }
+
+        // For debugging
+        private string GetFormValues(bool ignoreRequestVerificationToken = true)
+        {
+            string separator = " | ";
+            StringBuilder formDataSB = new ();
+
+            foreach (var key in this.Request.Form.Select(pair => pair.Key))
+            {
+                if (ignoreRequestVerificationToken && key == "__RequestVerificationToken")
+                {
+                    continue;
+                }
+                else
+                {
+                    formDataSB.Append(key);
+                    formDataSB.Append(": ");
+                    formDataSB.Append(Request.Form[key]);
+                    formDataSB.Append(separator);
+                }
+            }
+
+            string formData = formDataSB.ToString();
+            if (formData.EndsWith(separator))
+            {
+                formData = formData[..^separator.Length];
+            }
+
+            return formData;
+        }
+    }
+}
