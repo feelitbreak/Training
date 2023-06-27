ceedc122c6135ea376aff26e8d01fd3b315dced8
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-06-27 21:26:53 +0300

    codecademy routing

---
+++ ASP.NET/Codecademy/2. Page Models/2.2 Routing/2.2.5 asp-route-{value} revisited (full ActivityTracker project/Pages/Shared/_ProgressCirclePartialModel.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ActivityTracker.Pages
+{
+    public class ProgressCirclePartialModel : PageModel
+    {
+        public string BackgroundStroke { get; set; } = string.Empty;
+        public string ForegroundStroke { get; set; } = string.Empty;
+        public double PercentProgress { get; set; }
+        public double DisplayNumber { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public string IconUrl { get; set; } = string.Empty;
+    }
+}
