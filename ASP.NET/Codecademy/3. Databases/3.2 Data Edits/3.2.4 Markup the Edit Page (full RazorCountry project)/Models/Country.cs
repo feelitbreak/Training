3f343c9c2c002586de899cf331bd241d22b44505
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-07-01 21:33:20 +0300

    codecademy data edits

---
+++ ASP.NET/Codecademy/3. Databases/3.2 Data Edits/3.2.4 Markup the Edit Page (full RazorCountry project)/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RazorCountry.Models
+{
+    public class Country
+    {
+        public string ID { get; set; } = string.Empty;
+
+        public string ContinentID { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public int? Population { get; set; }
+
+        public int? Area { get; set; }
+
+        public DateTime? UnitedNationsDate { get; set; }
+    }
+}
